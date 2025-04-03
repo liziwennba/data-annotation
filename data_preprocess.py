10e82898274af5ed47634e6376e16a6c21bc29d3
Habseligkeiten87 <98534793+Habseligkeiten87@users.noreply.github.com>
2025-04-03 16:55:16 +0400

    Update data_preprocess.py

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,13 +1,15 @@
+import os
 import json
 import open3d as o3d
 import numpy as np
+
 for i in os.listdir('your/path/to/dataset'):
-    name = i 
-    with open(f'D:\\bendi\lidarSLAM\scenes\\{name}\\segments_anno.json','r') as f:
+    name = i
+    with open(f'your/path/to/dataset/{name}/segments_anno.json', 'r') as f:
         a = json.load(f)
-    pcd = o3d.io.read_point_cloud(f'D:\\bendi\lidarSLAM\scenes\\{name}\\mesh_aligned_0.05.ply')
+    pcd = o3d.io.read_point_cloud(f'your/path/to/dataset/{name}/mesh_aligned_0.05.ply')
     points = np.asarray(pcd.points)
     mask = np.zeros((points.shape[0]))
-    for i in a['segGroups']:
-        mask[np.array(i['segments'])] = i['objectId']
-        np.save(f'D:\\bendi\lidarSLAM\scenes\\{name}\\instance.npy',mask)
+    for seg in a['segGroups']:
+        mask[np.array(seg['segments'])] = seg['objectId']
+    np.save(f'your/path/to/dataset/{name}/instance.npy', mask)
