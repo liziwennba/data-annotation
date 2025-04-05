279d88a3caf84b4215ae0fa944b3d86a463ba76a
zhlhlhlhl <147411125+zhlhlhlhl@users.noreply.github.com>
2025-04-05 17:10:27 +0400

    Update data_preprocess.py

--- data_preprocess.py
+++ data_preprocess.py
@@ -3,13 +3,15 @@ import json
 import open3d as o3d
 import numpy as np
 
-for i in os.listdir('your/path/to/dataset'):
+dataset_path = 'your/path/to/dataset' #CHANGE HERE
+
+for i in os.listdir(dataset_path):
     name = i
-    with open(f'your/path/to/dataset/{name}/segments_anno.json', 'r') as f:
+    with open(f'{dataset_path}/{name}/segments_anno.json', 'r') as f:
         a = json.load(f)
-    pcd = o3d.io.read_point_cloud(f'your/path/to/dataset/{name}/mesh_aligned_0.05.ply')
+    pcd = o3d.io.read_point_cloud(f'{dataset_path}/{name}/mesh_aligned_0.05.ply')
     points = np.asarray(pcd.points)
     mask = np.zeros((points.shape[0]))
     for seg in a['segGroups']:
         mask[np.array(seg['segments'])] = seg['objectId']
-    np.save(f'your/path/to/dataset/{name}/instance.npy', mask)
+    np.save(f'{dataset_path}/{name}/instance.npy', mask)
